cecbe14d830870de3acad5e54176545f86b482f8
sunh423 <sunh423@gmail.com>
2021-06-26 08:50:57 -0700

    Main Roadmap

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-# here's main.py as made by Lucas Jensen
+#Discord Bot - Financial Portfolio Game
+
+#Queries/Commands
+    # $Next                             //Adds X amount of turns/years to the gameplay.
+    # $Start                            //Resets and starts the game for player.
+    # $Initialize                       //Initialize the bot/game on the server.
+    # $Balance                          //Checks the current balance/cash the player has.
+    # $Networth 
+    # $Age                              //Checks the age for the current player.
+    # $Assets                           //Shows the ratio and breakdown of the players' assets.
+    # $Projection                       //Projects the possible future state of the players' assets.
+    # $Preset [#]                       //Invest balance into asset allocation.
+
+#CSV, parsing Returns for projection.
+    # 9 different CSV's, 1 for each Asset Type.
+    # 
+
+#Gameplay
+    #Constant for when the person dies.
+    #Variable for bank balance; acculation per turn.
+    #Constant for inflation.
+    #PrevDictionary (Balance, + 9 AssetTypes, Age) 
+    #CurrDictionary (Balance, + 9 AssetTypes, Age)
