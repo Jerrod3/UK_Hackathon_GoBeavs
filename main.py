f025f29f325253f5fe5b24f657694b9a63253fb7
sunh423 <sunh423@gmail.com>
2021-06-26 14:19:37 -0700

    Added initial class for Players

--- main.py
+++ main.py
@@ -21,3 +21,20 @@
     #Constant for inflation.
     #PrevDictionary (Balance, + 9 AssetTypes, Age) 
     #CurrDictionary (Balance, + 9 AssetTypes, Age)
+
+class player:                    #Each player has their own gameInstance object created when calling $start
+    def __init__(self, name, age=18):        #__init__ is a object method that python automatically runs, when the instance/object is initialized/created
+        self.user = name
+        self.balanace = 100000            #Replace this with whatever the starting balance is.
+        self.age = age    #By default, this will be 18 unelss the parameter is overwritten
+
+        self._current_assets = dict()      #Storing all of the player's data as key:pair dictionary format.
+        self._previous_assets = dict()
+
+    def __repr__(self) -> str:       #__Repr__ is a method that tells python, what displays when you print or call the object directly.
+        return self.user
+
+Jerrod = player('Burbot#5573')
+print(Jerrod)            #Returns -> 'Burbot#5573'
+print(Jerrod.balanace)   #Returns -> '100000'
+
