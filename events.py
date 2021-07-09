78083318a391b3af7281eeb984a66eaac33aae18
Jerrod Lepper <lepperj@oregonstate.edu>
2021-07-09 02:51:12 -0400

    "Added some scenarios while waiting for the db to load"

--- events.py
+++ events.py
@@ -5,11 +5,13 @@
 VERY_GOOD_EVENTS = [
     "Harvard economists are surprised by the positive economic growth following the latest Lady Gaga album that inspired millions of consumers around the world to 'give back' during this pandemic."
     "Pfizer announces they've invited a vaccine that will prevent all viruses from spreading"
+    "90% of 1st world companies have opted for a 4 day work week, tripling economic production"
 ]
 
 GOOD_EVENTS = [
     "Immense quantity of gold dating back to 1800's was recently discovered in D.C."
     "Amazon Prime day overcomes Black Friday to become the biggest shopping day of the year."
+    "All world billionaires have pledged to donate 50% of their income to their respective countries' citizens"
 ]
 
 BAD_EVENTS = [
@@ -17,12 +19,14 @@ BAD_EVENTS = [
     "Oil prices rise sharply; families opt to stay home this shopping season."
     "Fed Chairman announces interest rates will double unexpectedly."
     "New trade policies raises consumer uncertainty to all time highs."
+    "Meme stonks have skyrocketed, destroying 80% of pension funds"
 ]
 
 VERY_BAD_EVENTS = [
     "Millions of Americans disappointed as a handful of U.S. Senators have been found guilty of accepting unwarranted bribes in the forms of bitcoins."
     "Housing market crashes. Unemployment at all time highs."
     "Software glitches cause markets to tumble; investers flee markets at record rates."
+    "Canada has invaded the United States for its maple syrup reserves, WW3 has begun"
 ]
 
 #If the yearly profitted over 3 percent in return, randomly pull an event text from the VERY_GOOD_EVENTS list.
